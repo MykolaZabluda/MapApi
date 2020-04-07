6071cd89f4b3b978730003fc832280a0ab1f4b5f
juliret <yulia.rekuta996@gmail.com>
2020-04-08 02:50:36 +0300

    Marker is works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
+import { Marker } from '@agm/core';
+import { MouseEvent } from '@agm/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MapApi';
+
+export class AppComponent{
+  lat = 49.232834;
+  lng = 28.469158;
+  zoom = 5;
+
+   markers: marker[] = [
+      {
+         lat: 49.232834,
+         lng: 28.469158,
+         name: 'town',
+         id: 1
+      },
+      {
+         lat: 49.224555,
+         lng: 28.419033,
+         name: 'cinema1',
+         id: 2
+      },
+      {
+         lat: 49.233295,
+         lng: 28.465603,
+         name: 'library',
+         id: 3
+      },
+      {
+         lat: 49.239429,
+         lng: 28.510384,
+         name: 'train_station',
+         id: 4
+      },
+      {
+         lat: 49.233672,
+         lng: 28.411643,
+         name: 'university',
+         id: 5
+      },
+      {
+         lat: 49.233637,
+         lng: 28.466240,
+         name: 'cinema2',
+         id: 6
+      },
+   ];
+
+   public selectedMarker: marker = this.markers[0];
+
+   onChange(value){
+      console.log(value.name);
+      this.selectedMarker = value;
+   }
 }
+
+// tslint:disable-next-line: class-name
+interface marker {
+   lat: number;
+   lng: number;
+   name: string;
+   id: number;
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AgmCoreModule} from '@agm/core';
+
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCWNlsBRdG1gqntKcuPn-NdKw4jsdTRz80'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
